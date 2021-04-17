1c406204ad988151a94d8ae94d707ea2c6391738
Nazeeeh <nazihahmed55@gmail.com>
2021-04-17 13:37:25 +0100

    Update the joke api

---
+++ components/joke.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="joke">
+        {{joke}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Joke',
+  props: ['joke', 'id']
+}
+</script>
+
+<style>
+    .joke{
+        margin: 1rem 0;
+        border: 1px dotted #ccc;
+        padding: 1rem;
+    }
+</style>
