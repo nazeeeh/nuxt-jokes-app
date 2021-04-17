55df1b26a63933d1ff16d5dbb9f6c50da1fab051
Nazeeeh <nazihahmed55@gmail.com>
2021-04-17 14:11:51 +0100

    Route added to api

--- components/joke.vue
+++ components/joke.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="joke">
+    <nuxt-link :to="'jokes/' + id">
+        <div class="joke">
         {{joke}}
     </div>
+    </nuxt-link>
 </template>
 
 <script>
---
+++ pages/jokes/_id/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <nuxt-link to="/jokes"> Back to jokes
+        </nuxt-link>
+        <h4>{{joke}}</h4>
+        <hr />
+        <small>Joke:ID {{$route.params.id}}</small>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      joke: {}
+    }
+  },
+  async created () {
+    const config = {
+      headers: {
+        Accept: 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config)
+      this.joke = res.data.joke
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
